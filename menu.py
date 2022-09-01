ee345159debfc568ee0a25894086f5bf98765197
PaulAllan209 <dpaulallan15@gmail.com>
2022-09-01 16:43:47 +0800

    Added new images

--- menu.py
+++ menu.py
@@ -6,22 +6,39 @@ class Menu:
         self.game = game
         self.mid_w, self.mid_h = self.game.DISPLAY_W/2, self.game.DISPLAY_H/2
         self.run_display = True
+        self.bg_img_loc = 'assets/sent_images'
        
 
     def blit_canvas(self):
         self.game.window.blit(self.game.canvas, (0,0))
         pygame.display.update()
 
+    def bg_loader(self, img, scale):
+        W = img.get_width()
+        H = img.get_height()
+        image = pygame.transform.scale(img, (int(W*scale), int(H*scale)))
+        image_rect = image.get_rect()
+        image_rect.topleft = (0, 0)
+        self.draw_bg(image, image_rect)
+
+    def draw_bg(self, image, image_rect):
+        self.game.canvas.blit(image, image_rect.topleft)
+
+
 
 class MainMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game)
         self.menu_button = ImportMainMenuButton(self)
+        self.bg_img = pygame.image.load(f'{self.bg_img_loc}/bg1.png').convert_alpha()
+
 
     def display_menu(self):
         self.run_display = True
         while self.run_display:
-            self.game.canvas.fill(self.game.VIOLET)
+            # self.game.canvas.fill(self.game.VIOLET)
+            self.bg_loader(self.bg_img, 1)
+
             self.game.check_events()
             self.menu_button.import_menu_button()
             self.blit_canvas()
